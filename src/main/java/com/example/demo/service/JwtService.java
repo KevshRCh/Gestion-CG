e335461c2896d1b3eb0ef6f02704dc815e94e3e4
KevshRCh <kevinrch17@gmail.com>
2025-11-12 21:25:43 -0500

    Gestion CG

---
+++ src/main/java/com/example/demo/service/JwtService.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+	private static final String SECRET_KEY = "mi_clave_super_secreta_para_jwt_segura_2025_1234567890";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hora
+
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generarToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extraerUsuario(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
